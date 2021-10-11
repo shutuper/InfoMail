fa983562b3e10354373e08455df77009f645e72a
shut_uper <ttatta3adpot@gmail.com>
2021-10-11 22:43:15 +0300

    dev/ added some app properties

--- src/test/java/com/infopulse/infomail/InfoMailApplicationTests.java
+++ src/test/java/com/infopulse/infomail/InfoMailApplicationTests.java
@@ -1,16 +1,9 @@
 package com.infopulse.infomail;
 
-import com.infopulse.infomail.configuration.InfoMailConfiguration;
 import com.infopulse.infomail.services.mail.EmailSenderService;
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest()
 class InfoMailApplicationTests {
