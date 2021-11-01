4f147958f35283899aa4aa81fc2de1c0a5fee899
Lastzlo <z.last.zlo@gmail.com>
2021-11-01 17:51:44 +0200

    feat InformationController

---
+++ src/main/java/com/infopulse/infomail/controllers/rest/InformationController.java
@@ -0,0 +1,20 @@
+package com.infopulse.infomail.controllers.rest;
+
+import com.infopulse.infomail.models.ServerInformation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/information")
+@CrossOrigin(origins = "http://localhost:4200")
+public class InformationController {
+
+    @GetMapping
+    public ResponseEntity getInformation() {
+        return ResponseEntity.ok(new ServerInformation("Welcome to the InfoMail project!"));
+    }
+
+}
