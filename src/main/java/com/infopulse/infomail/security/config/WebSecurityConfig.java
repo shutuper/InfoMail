9a0d031e6f828eb8d68e9a7319a919f1053a3d80
Lastzlo <z.last.zlo@gmail.com>
2021-11-01 17:53:21 +0200

    refactor comment out corsFilter

--- src/main/java/com/infopulse/infomail/security/config/WebSecurityConfig.java
+++ src/main/java/com/infopulse/infomail/security/config/WebSecurityConfig.java
@@ -60,17 +60,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return provider;
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http//localhost:4200");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
+	//don't work correct
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("http//localhost:4200");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		source.registerCorsConfiguration("/**", config);
+//		return new CorsFilter(source);
+//	}
 
 	@Bean
 	@Override
