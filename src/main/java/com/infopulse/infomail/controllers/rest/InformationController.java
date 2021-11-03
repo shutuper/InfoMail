a0ee2503702ad599d95066480e6680febf5ac3e2
Lastzlo <z.last.zlo@gmail.com>
2021-11-03 19:22:34 +0200

    refactor remove @CrossOrigin annotation

--- src/main/java/com/infopulse/infomail/controllers/rest/InformationController.java
+++ src/main/java/com/infopulse/infomail/controllers/rest/InformationController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/information")
-@CrossOrigin(origins = "http://localhost:4200")
 public class InformationController {
 
     @GetMapping
