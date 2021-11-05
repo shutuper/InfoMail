e44c1097878c1bcc13312a785b3b11b315626266
Lastzlo <z.last.zlo@gmail.com>
2021-11-05 22:55:56 +0200

    add RepeatType

---
+++ src/main/java/com/infopulse/infomail/models/messages/RepeatType.java
@@ -0,0 +1,11 @@
+package com.infopulse.infomail.models.messages;
+
+public enum RepeatType {
+    NOTHING,
+    ON_WORK_DAYS,
+    EVERY_DAY,
+    EVERY_WEEK,
+    EVERY_MONTH,
+    EVERY_YEAR,
+    OTHER
+}
