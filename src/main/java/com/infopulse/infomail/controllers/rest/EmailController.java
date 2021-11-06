0b6f209e10c4fc23741394d8e6ed2747c5861c88
Lastzlo <z.last.zlo@gmail.com>
2021-11-06 19:11:04 +0200

    feat add EmailController

---
+++ src/main/java/com/infopulse/infomail/controllers/rest/EmailController.java
@@ -0,0 +1,21 @@
+package com.infopulse.infomail.controllers.rest;
+
+import com.infopulse.infomail.dto.email.EmailDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emails")
+public class EmailController {
+
+    @PostMapping
+    public ResponseEntity<EmailDTO> addEmail(@RequestBody EmailDTO email) {
+        System.out.println("email = " + email);
+        return new ResponseEntity<>(email, HttpStatus.CREATED);
+    }
+
+}
