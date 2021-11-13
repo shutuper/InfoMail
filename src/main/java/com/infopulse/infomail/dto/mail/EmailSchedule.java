b8a3e4ae59ffe1c5c0e82cd9a7aa85f56e0027dc
Lastzlo <z.last.zlo@gmail.com>
2021-11-13 04:26:13 +0200

    feat add toDTO and fromDTO

--- src/main/java/com/infopulse/infomail/dto/mail/EmailSchedule.java
+++ src/main/java/com/infopulse/infomail/dto/mail/EmailSchedule.java
@@ -4,11 +4,11 @@ import com.infopulse.infomail.models.mail.Schedule;
 import com.infopulse.infomail.models.mail.enums.RepeatType;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Getter
@@ -30,4 +30,37 @@ public class EmailSchedule implements Schedule {
 	public EmailSchedule() {
 	}
 
+	public EmailScheduleDTO toDTO() {
+		EmailScheduleDTO dto = new EmailScheduleDTO();
+		dto.setSendNow(this.sendNow);
+
+		if(this.sendDateTime !=null) dto.setSendDateTime(Timestamp.valueOf(this.sendDateTime));
+		dto.setRepeatAt(this.repeatAt);
+		if(this.endDate !=null) dto.setEndDate(Timestamp.valueOf(LocalDateTime.of(this.endDate, LocalTime.now())));
+
+		dto.setDaysOfWeek(this.daysOfWeek);
+		dto.setDayOfMonth(this.dayOfMonth);
+		dto.setDayOfWeek(this.dayOfWeek);
+		dto.setNumberOfWeek(this.numberOfWeek);
+		dto.setMonth(this.month);
+
+		return dto;
+	}
+
+	public static EmailSchedule fromDTO(EmailScheduleDTO dto) {
+		EmailSchedule schedule = new EmailSchedule();
+		schedule.setSendNow(dto.isSendNow());
+
+		if(dto.getSendDateTime() !=null) schedule.setSendDateTime(dto.getSendDateTime().toLocalDateTime());
+		schedule.setRepeatAt(dto.getRepeatAt());
+		if(dto.getEndDate() !=null) schedule.setEndDate(dto.getEndDate().toLocalDateTime().toLocalDate());
+
+		if(dto.getDaysOfWeek() !=null) schedule.setDaysOfWeek(dto.getDaysOfWeek());
+		schedule.setDayOfMonth(dto.getDayOfMonth());
+		schedule.setDayOfWeek(dto.getDayOfWeek());
+		schedule.setNumberOfWeek(dto.getNumberOfWeek());
+		schedule.setMonth(dto.getMonth());
+
+		return schedule;
+	}
 }
