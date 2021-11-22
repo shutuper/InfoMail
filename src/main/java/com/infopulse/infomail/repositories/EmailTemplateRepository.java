6539d3abba563ac9fbdad9cb9f12afa9832274c0
Lastzlo <z.last.zlo@gmail.com>
2021-11-22 23:50:34 +0200

    feat add findAllByAppUser_Email

--- src/main/java/com/infopulse/infomail/repositories/EmailTemplateRepository.java
+++ src/main/java/com/infopulse/infomail/repositories/EmailTemplateRepository.java
@@ -5,11 +5,12 @@ import com.infopulse.infomail.models.mail.EmailTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
 
 	Optional<EmailTemplate> findById(Long id);
-
+	List<EmailTemplate> findAllByAppUser_Email(String email);
 }
