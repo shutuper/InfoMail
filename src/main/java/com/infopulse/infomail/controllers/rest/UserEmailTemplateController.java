3e14694b09374b97ebd12f2c96d34b5f2bc71c07
Lastzlo <z.last.zlo@gmail.com>
2021-11-26 18:23:38 +0200

    refactor set mapping `usertemplates`

--- src/main/java/com/infopulse/infomail/controllers/rest/UserEmailTemplateController.java
+++ src/main/java/com/infopulse/infomail/controllers/rest/UserEmailTemplateController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/templates")
+@RequestMapping("/api/v1/usertemplates")
 @AllArgsConstructor
 public class UserEmailTemplateController {
 
