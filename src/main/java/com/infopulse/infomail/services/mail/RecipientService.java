354cc04206a665f21765be6cb1d7d3846a9716d7
Lastzlo <z.last.zlo@gmail.com>
2021-12-04 23:59:14 +0200

    dev/ get all recipients by job name

--- src/main/java/com/infopulse/infomail/services/mail/RecipientService.java
+++ src/main/java/com/infopulse/infomail/services/mail/RecipientService.java
@@ -1,9 +1,9 @@
 package com.infopulse.infomail.services.mail;
 
 import com.infopulse.infomail.dto.api.emails.RecipientDTO;
-import com.infopulse.infomail.models.recipients.AppRecipient;
 import com.infopulse.infomail.models.mail.AppUserEmailsInfo;
 import com.infopulse.infomail.models.mail.enums.RecipientType;
+import com.infopulse.infomail.models.recipients.AppRecipient;
 import com.infopulse.infomail.repositories.AppRecipientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,14 @@ public class RecipientService {
 
 	private final AppRecipientRepository recipientRepository;
 
+
+	public List<RecipientDTO> getAllAsDTOByJobName(String jobName) {
+		List<AppRecipient> appRecipients = recipientRepository.findAllByUserInfo_QrtzJobDetail_JobName(jobName);
+		if (appRecipients.isEmpty())
+			throw new IllegalStateException(String.format("There are no recipients with QrtzJobDetail_JobName: %s", jobName));
+		return appRecipients.stream()
+				.map(x -> new RecipientDTO(x.getEmail(), x.getRecipientType())).toList();
+	}
 
 	public List<AppRecipient> getAllByUserInfoId(Long userInfoId) {
 		List<AppRecipient> recipients = recipientRepository.findAllByUserInfo_Id(userInfoId);
